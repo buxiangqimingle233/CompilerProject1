9fa0f6f2441d7ee45243451852c63bdedac728d1
SocordiaZhang <59304418+SocordiaZhang@users.noreply.github.com>
2020-05-16 03:28:22 +0800

    Add files via upload

---
+++ project1/solution/solution.cc
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "json/json.h"
+
+#include "IR.h"
+#include "IRMutator.h"
+#include "IRVisitor.h"
+#include "IRPrinter.h"
+#include "type.h"
+
+using namespace Boost::Internal;
+
+void translate(std::string ipath, std::string opath) {
+
+	// read json file and parse it
+	std::string name, data_type, kernel;
+	std::vector<std::string> ins, outs;
+	std::ifstream ifile(ipath, std::ios::binary);
+	if (!ifile) return;
+
+	Json::Reader reader;
+    Json::Value root;
+
+	if (reader.parse(ifile, root)) {
+		name = root["name"].asString();
+		for (int i = 0; root["ins"][i].asString() != ""; i++)
+			ins.push_back(root["ins"][i].asString());
+		for (int i = 0; root["outs"][i].asString() != ""; i++)
+			outs.push_back(root["outs"][i].asString());
+		data_type = root["data_type"].asString();
+		kernel = root["kernel"].asString();
+	}
+
+	ifile.close();
+
+
+	std::string code = "";
+	/////
+
+	/////
+
+	std::ofstream ofile(opath, std::ios::out);
+	ofile << code;
+	ofile.close();
+	
+}
+
+int main() {
+	for (int i = 1; i <= 10; i++) {
+		std::string ipath = "./cases/case" + std::to_string(i) + ".json";
+		std::string opath = "./kernels/kernel_case" + std::to_string(i) + ".cc";
+		translate(ipath, opath);
+	}
+	return 0;
+}
